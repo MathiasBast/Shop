a82f69462cd5eddf96da92284e2bbbd202961c09
Mathias Bast <mathiasbast123@gmail.com>
2020-09-04 00:12:21 +1200

    inital commit;

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Shop
+    {
+        private List<Item> items;
+        private int maxContacts;
+        public Shop (int maxContacts)
+        {
+            items = new List<Item>(maxContacts);
+            this.maxContacts = maxContacts;
+        }
+
+    }
+    class Item
+    {
+
+    }
+}
